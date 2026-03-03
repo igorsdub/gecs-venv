ad4d76b3c0cee5cb486195e4ba33e084c87d60f1
Igors Dubanevics <26872977+igorsdub@users.noreply.github.com>
2026-03-03 14:29:56 +0900

    Remove shell scripts

--- scripts/get_summary.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# This script extracts summary information from the header of a given file.
-# Usage: bash scripts/get_summary.sh <filename>
-# Example: bash scripts/get_summary.sh data/dracula.txt
-
-head -n 17 $1 | tail -n 8 | grep Title
-head -n 17 $1 | tail -n 8 | grep Author
-head -n 17 $1 | tail -n 8 | grep Language
-head -n 17 $1 | tail -n 8 | grep Release
